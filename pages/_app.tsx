665f12748abeeed11fa2ccc7d1c1e702626334a3
Luis Henrique <luishcc2003@gmail.com>
2023-10-14 13:40:38 -0300

    Adicionando GlobalStyles.tsx com o reset.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
-import '@/styles/globals.css'
+import 'styles/globals.css'
 import type { AppProps } from 'next/app'
+import GlobalStyles from 'styles/GlobalStyles'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <>
+        <>  
+            <GlobalStyles/>
             <Component {...pageProps} />
         </>
     )
