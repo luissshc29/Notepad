f8649b9bab00e75c35827090d1fc135f37153bcf
Luis Henrique <luishcc2003@gmail.com>
2024-07-21 21:50:26 -0300

    Adicionando link para o portfolio no rodape

--- src/components/Rodape/index.tsx
+++ src/components/Rodape/index.tsx
@@ -1,21 +1,36 @@
+import React from "react";
 import {
   AiOutlineInstagram,
   AiOutlineLinkedin,
   AiOutlineGithub,
 } from "react-icons/ai";
+import { FiExternalLink } from "react-icons/fi";
 
 export default function Rodape() {
   return (
-    <footer className="mt-8 w-full flex justify-center pt-8 font-sans gap-2 items-center text-slate-600 lg:col-span-2">
-      <p className="h-full">Feito por Luis Henrique |</p>
+    <footer className="flex justify-center items-center gap-2 lg:col-span-2 mt-auto mb-0 py-6 w-full font-sans text-base text-neutral-500">
+      <div className="flex items-center gap-[3px] h-full">
+        Feito por{" "}
+        <a
+          href="https://luishenrique-dev.com.br/"
+          className="flex items-center gap-[3px] underline"
+          target="_blank"
+        >
+          Luis Henrique <FiExternalLink />
+        </a>{" "}
+        |
+      </div>
       <div className="flex gap-2">
-        <a href="https://www.instagram.com/luissshc_/">
+        <a href="https://www.instagram.com/luissshc_/" target="_blank">
           <AiOutlineInstagram />
         </a>
-        <a href="https://www.linkedin.com/in/luis-henrique-6a7425165/">
+        <a
+          href="https://www.linkedin.com/in/luis-henrique-6a7425165/"
+          target="_blank"
+        >
           <AiOutlineLinkedin />
         </a>
-        <a href="https://github.com/luissshc29">
+        <a href="https://github.com/luissshc29" target="_blank">
           <AiOutlineGithub />
         </a>
       </div>
